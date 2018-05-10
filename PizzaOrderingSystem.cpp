079b5ed5d22896c9133e893302ba5d3a4df8ddf0
Ahmed Aboelyazeed <ahmedalarabe5@gmail.com>
2018-05-10 02:12:10 +0200

    added menu & fixed some invalid inputs

--- PizzaOrderingSystem.cpp
+++ PizzaOrderingSystem.cpp
@@ -3,29 +3,33 @@
 #include <ctime>
 #include <cstdlib> 
 #include <fstream>
+#include <ctype.h>
 using namespace std;
 //structs---------------------------------------------------------------------=
 struct Salesperson {
     string name;
-    char passchar;
+    string passchar;
 };
 struct Pizza {
     string nameOfCustomer;
     string pizzaKind;
     string size;
     string status;
-    int priceOfOnePiece;
+    //int priceOfOnePiece;
     int numberOfPieces;
-    char coupon;
+    //char coupon;
     int couponSize;
-    string systemTime;  
+    string specialOrder;
     string topping;
     string dessert;
     int paid;
 };
 //global Vars-------------------------------------------------------------=
 struct Pizza Pizzas[1000];
-int index=0; 
+int index=0;
+string menu[10] = {"Berry","Arugula","Prosciutto","Macaroni","Cheese","Cantaloupe","Sweet Ricotta","Chicken Alfredo","null","null"};
+int prices[10] = {15,24,56,35,74,83,96,25,0,0}; 
+int numberMenu=8;
 //functions Prototypes-----------------------------------------------------=  
 void newPizza();
 void checkStatus();
@@ -35,6 +39,7 @@ bool isValid(string coupon);
 void history();
 void printToFile();
 int captureCouponSize(string CouponID);
+void showPizzaMenu();
 //---------------------------------------------------------=
 int main(){    
 bool isManager=false; 
@@ -45,8 +50,8 @@ cout<<"\n\t\t=========The Login Window=========\n\t";
 cout<<"\n\t\t\tName : ";
 getline(cin,Salespersons[0].name);
 cout<<"\n\t\t\tPassChar : ";
-cin>>Salespersons[0].passchar;
-if(Salespersons[0].passchar=='M'){
+getline(cin,Salespersons[0].passchar);
+if(Salespersons[0].passchar=="M"){
     isManager=true;
     cout<<"\n\t\t\tWelcome Back Manager!!\n";
     goto MainScreen;
@@ -64,8 +69,15 @@ while(1){
         cout<<"\n\t\t||  [6]Print the History to an external file  ||";
     }
     cout<<"\n\t\t||  [0]EXIT                                   ||";
-    cout<<"\n\t\t\tYour Choice : ";
-    cin>>choice;
+    while(1){
+        cout<<"\n\t\t\tYour Choice : ";
+        cin>>choice;
+        if(choice>=0&&choice<=6){
+            break;
+        }else{
+            continue;
+        }
+    }
     switch(choice){
         case 0: 
                 cout<<"\n\t\t\t============="; 
@@ -107,12 +119,14 @@ if(choice==0){
 }
 //-------------------------------------------------------------------------=
  void newPizza(){
-    char specialOrder;
+    string specialOrder;
     bool isSpecialOrder=false;
     string couponID;
-    char isFinal;
-    char newOrder;
+    string isFinal;
+    string newOrder;
     char wantDessert;
+    int ofMenu;
+    string hasCoupon;
 
      for(int i=index;i<1000;i++){
        /*
@@ -121,37 +135,58 @@ if(choice==0){
         string* tm=ctime(&now);
         strcpy(tm,Pizzas[i].systemTime);
         */
-        cout<<"\n\t\tIs this a special order(Y|N)?";
-        cin>>specialOrder;
+        while(1){
+            cout<<"\n\t\tIs this a special order(Y|N)?";
+            cin>>specialOrder;
+            if(specialOrder=="Y"||specialOrder=="N"){
+                break;   
+            }else{
+                cout<<"\n\t\t\tInvalid Input";
+                continue;
+            }   
+        }
+        if(specialOrder=="Y"){
+            Pizzas[i].specialOrder="Yes";
+        }else if(specialOrder=="N"){
+            Pizzas[i].specialOrder="No";
+        }
+
         cout<<"\n\t\tName of the Customer : ";
         cin>>Pizzas[i].nameOfCustomer;
-        cout<<"\n\t\tPizza Kind : ";
-        cin>>Pizzas[i].pizzaKind;
+        cout<<"\n\t\tWe have : ";
+        showPizzaMenu();
+        cout<<"\n\t\tYour choice : ";
+        cin>>ofMenu;
+        Pizzas[i].pizzaKind=menu[ofMenu-1];
         cout<<"\n\t\tSmall-Medium-Big";
         cout<<"\n\t\tPizza Size : ";
-        //cout<<"\t\t";
         cin>>Pizzas[i].size;
-        if(specialOrder=='Y'){
+        if(specialOrder=="Y"){
             cout<<"\n\t\t Available : 1,2,3,4,5";
             cout<<"\n\t\t please choose the topping you want : ";
-            //cout<<"\t\t";
             cin>>Pizzas[i].topping;
             cout<<"\n\t\tDo you want any dessert(Y for Yes)?";
             cin>>wantDessert;
             if(wantDessert=='Y'){
                 cout<<"\n\t\t Available : 1,2,3,4,5";
                 cout<<"\n\t\tplease choose the dessert you want : ";
-                //cout<<"\t\t";
                 cin>>Pizzas[i].dessert;
             }
         }
+        nospecial:
         cout<<"\n\t\tNumber of pieces : ";
         cin>>Pizzas[i].numberOfPieces;
-        cout<<"\n\t\tPrice of one piece : ";
-        cin>>Pizzas[i].priceOfOnePiece;
-        cout<<"\n\t\tAvaliablity of Coupons(Y|N) : ";
-        cin>>Pizzas[i].coupon;
-        if(Pizzas[i].coupon=='Y'){
+        //cout<<"\n\t\tPrice of one piece : ";
+        //cin>>Pizzas[i].priceOfOnePiece;
+        while(1){
+            cout<<"\n\t\tAvaliablity of Coupons(Y|N) : ";
+            cin>>hasCoupon;
+            if(hasCoupon=="Y"||hasCoupon=="N"){
+                break;
+            }else{
+                cout<<"\n\t\tEnter Either \"Y\" or \"N\"!!";
+                continue;
+        }
         while(1){  
             cout<<"\n\t\tCoupon ID : ";
             cin>>couponID;
@@ -167,28 +202,43 @@ if(choice==0){
         cout<<"\n\t\tOrder Status : ";
         //cout<<"\t\t";
         cin>>Pizzas[i].status;
-        Pizzas[i].paid=Pizzas[i].priceOfOnePiece*Pizzas[i].numberOfPieces-captureCouponSize(couponID);
+        Pizzas[i].paid=prices[ofMenu-1]*Pizzas[i].numberOfPieces-captureCouponSize(couponID);
         cout<<"\n\t\tThis Customer Gotta pay : "<<Pizzas[i].paid;
-        cout<<"\n\t\tIs this FINAL(Y|N)?";
-        cin>>isFinal;
-        if(isFinal=='Y'){
+        while(1){
+            cout<<"\n\t\tIs this FINAL(Y|N)?";
+            cin>>isFinal;
+            if(isFinal=="Y"||isFinal=="N"){
+                break;
+            }else{
+                cout<<"\n\t\tEnter Either \"Y\" or \"N\"!!";
+                continue;
+            }
+        }
+        if(isFinal=="Y"){
             cout<<"\n\t\tYou Just Sumbitted a new Order!!";
-            cout<<"\n\t\tDo you want to sumbit another order(Y|N)?";
+            while(1){
+                cout<<"\n\t\tDo you want to sumbit another order(Y|N)?";
             cin>>newOrder;
-            if(newOrder=='Y'){
+            if(newOrder=="Y"||newOrder=="N"){
+                break;
+            }else{
+            cout<<"\n\t\tEnter Either \"Y\" or \"N\"!!";
+            continue;
+        }
+            }
+            if(newOrder=="Y"){
                 index++;
                 continue;
-            }else if(newOrder=='N'){
+            }else if(newOrder=="N"){
                 index++;
                 break;
             }
-            }else if(isFinal=='N'){
+            }else if(isFinal=="N"){
                 index--;
                 continue;
         }
 
      }
-     //ToDO: determine the discount from the CouponID automatically
  }
  //----------------------------------------------------------------------=
  void checkStatus(){
@@ -206,8 +256,16 @@ if(choice==0){
              break;
          }
      }
-       cout<<"\n\t\tDo you want to check another order(0 to return)?";
-       cin>>anotherChecking;
+        while(1){ 
+            cout<<"\n\t\tDo you want to check another order(0 to return)?";
+            cin>>anotherChecking;
+            if(anotherChecking==0){
+                break;
+            }else{
+                cout<<"\n\t\tYou can only enter \"0\"!";
+                continue;
+            }
+        }
      }
      //TODO: sensitivity for duplicate names
  }
@@ -231,9 +289,16 @@ if(choice==0){
              break;
          }
         
+     }  while(1){
+            cout<<"\n\t\tDo you want to change another Status(0 to return)?";
+            cin>>anotherChange;
+            if(anotherChange==0){
+                break;
+            }else{
+                cout<<"\n\t\tYou can only enter \"0\"!";
+                continue;
+            }
      }
-        cout<<"\n\t\tDo you want to change another Status(0 to return)?";
-        cin>>anotherChange;
      }
           //TODO: sensitivity for duplicate names
  }
@@ -246,16 +311,33 @@ if(choice==0){
             cout<<"\n"<<"Customer name : "<<Pizzas[i].nameOfCustomer<<" || Pizza Kind : "<<Pizzas[i].pizzaKind<<" || Size : "<<Pizzas[i].size<<" || Status : "<<Pizzas[i].status<<" || Paid : "<<Pizzas[i].paid;
             //TODO: Displaying if the customer had a special order or not
          }
-         cout<<"\n\t\tTo return Enter 0 : ";
-         cin>>toreturn;
+        while(1){
+            cout<<"\n\t\tTo return Enter 0 : ";
+            cin>>toreturn;
+            if(toreturn==0){
+                break;
+            }else{
+                cout<<"\n\t\tYou can only enter \"0\"!";
+                continue;
+            }
+        }
      }
  }
 //-------------------------------------------------------------------------------=
 void generateCoupon(){
     int sizeOfCoupon;
     string couponID;
-    cout<<"\n\t\tEnter the Size of the coupon to be generated : ";
-    cin>>sizeOfCoupon;
+    while(1){
+        cout<<"\n\t\tEnter the Size of the coupon to be generated : ";
+        cin>>sizeOfCoupon;
+        if(isdigit(sizeOfCoupon)){
+            break;
+        }else{
+            cout<<"\n\t\tPlease enter a proper size!";
+            continue;
+        }
+
+    }
     srand((unsigned)time(0)); 
     int randomnum = rand()%1000; 
     couponID="Pi-"+to_string(randomnum)+'.'+to_string(sizeOfCoupon)+'z';
@@ -280,10 +362,20 @@ void printToFile(){
     history.close();
     cout<<"Done Printing To File";     
 }
-//imp TODO: names with spaces in the newPizza function
+//imp TODO: names with spaces in the newPizza function 
 //------------------------------------------------------------------------------------------=
 int captureCouponSize(string couponID){
-int couponSize;
-couponSize = atoi(couponID.substr(7,couponID.find("z")).c_str());
-    return couponSize;
+    int couponSize;
+    couponSize = atoi(couponID.substr(couponID.find(".")+1,couponID.find("z")).c_str());
+        return couponSize;
+}
+//---------------------------------------------------------------------------------------=
+void showPizzaMenu(){
+    for(int i =0;i<10;i++){
+        if(i%2==0&&menu[i]!="null"){
+            cout<<"\n\t\t\t>> "<<"["<<i+1<<"]"<<menu[i]<<" - ";
+        }else if(i%2!=0&&menu[i]!="null"){
+            cout<<"["<<i+1<<"]"<<menu[i];
+        }
+    }
 }
